75940c930b614faef91c2ef55e833de1edea2900
bassem eloraby <BASEM12683@GMAIL.COM>
2023-05-11 11:10:57 +0300

    cors

--- readme.md
+++ readme.md
@@ -1,8 +1,12 @@
 https://www.youtube.com/watch?v=CvCiNeLnZ00
 00:18:00
+00:38:00
+00:40:00
 ====================================================
 terminal
 ----------
 npm init -y
 npm i express
 npm i -D nodemon
+npm i date-fns uuid
+npm i cors
--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const cors = require('cors')
 
-const PORT = process.env.PORT || 3500
+const app = express();
+const path = require('path');
+const { logger } = require('./middleware/logger');
+const PORT = process.env.PORT || 3500;
+app.use(logger);
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.use('/', require('./routes/root'))
+app.use('/', require('./routes/root'));
 
 app.all('*', (req, res) => {
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' })
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
-})
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
